60236eb0cef2f3420a273db9249a625d216bca07
Jose Luis Perez Diez <jluis@escomposlinux.org>
2019-05-05 15:44:25 +0200

    Grammar added

--- lib/xes2pdf.pm
+++ lib/xes2pdf.pm
@@ -5,9 +5,99 @@ use warnings;
 use English;
 use feature qw(signatures);
 no warnings qw(experimental::signatures);
+use Marpa::R2;
 
 
 our $VERSION = "0.01";
+
+my $gramar = <<'SOURCE';
+
+:start          ::= spec
+
+spec            ::= <spec decl>+ action => do_list
+<spec decl>     ::= <jobless decl> | <job decl>
+<jobless decl>  ::= <command> #action => fixed_Command
+<jobless decl>  ::= <data list> action => do_list
+<jobless seq>   ::= <jobless decl>* action => do_list
+<job decl>      ::= <job command> <jobless seq> action => do_job
+
+<anyChar>       ~ [\x{0}-\x{ffff}]
+
+<data>          ~ <anyChar>
+
+<data list>     ::= <data>+ action => misdatos
+                    
+                    
+
+<job command>   ::= <reset> action=>do_job
+<job command>   ::= <PrintJob> action=>do_job
+<job command>   ::= <font> action => font
+<command>       ::= <font use> action => font
+                    |<efects> action => efect
+                    |<offset>|<unit6>|<unit3>|<eraseTabs>
+                    |<eraseVtabs>|<start_justify>
+                    |<stop_justify>|<center>|<start_merge>
+                    |<stop_merge>|<startsust>
+                    |<line> action => draw
+                    |<curposabs> action => move
+                    |<margins> action => margin
+                    |<Intray> action => tray
+
+<efects>        ::= <start_bold>|<stop_bold>|
+                    <stop_over>|<start_over>|
+                    <start_line>|<stop_line>|
+                    <subindex>|<baseline>|<superindex>
+                    
+
+#:lexeme        ~ <UDK>   pause => before priority=>20
+#:lexeme        ~ <reset> priority=>10 pause => after
+#:lexeme        ~ <data> pause => before priority=>-1
+
+<reset>         ~ <escape>'+X'<lineEnd>
+<reset>         ~ <escape>'+X'<comment><lineEnd>
+<PrintJob>	    ~ <escape>'+P'<lineEnd>
+<PrintJob>      ~ <escape>'+P'<comment><lineEnd>
+<font>          ~ <escape>'+'[\d] <value><lineEnd>
+<font use>      ~ <escape>[\d]
+<start_bold>    ~ <escape>'b'
+<stop_bold>     ~ <escape>'p'
+<stop_over>     ~ <escape>'zp'
+<start_over>    ~ <escape>'zo'<anyChar>
+<start_line>    ~ <escape>'u'
+<stop_line>     ~ <escape>'w'
+<subindex>      ~ <escape>'l'
+<baseline>      ~ <escape>'s'
+<superindex>    ~ <escape>'h'
+<offset>        ~ <escape>'o'
+<unit6>         ~ <escape>'zg'
+<unit3>         ~ <escape>'zf'
+<eraseTabs>     ~ <escape>'d'
+<eraseVtabs>    ~ <escape>'e'
+<start_justify> ~ <escape>'j'
+<stop_justify>  ~ <escape>'k'
+<center>        ~ <escape>'q'
+<start_merge>   ~ <escape>'ze'
+<stop_merge>    ~ <escape>'zd'
+<startsust>     ~ <escape>'zt'
+<line>          ~ <escape>[xy] <X> <coma> <Y> <coma> <length> <coma> <thick> <value> <lineEnd>
+<line>          ~ <escape>[xy]<coma> <X> <coma> <Y> <coma> <length> <coma> <thick> <value> <lineEnd>
+<curposabs>     ~ <escape>'a'<X> <coma> <Y> <lineEnd>
+<margins>       ~ <escape>'m'<digits><coma> <digits> <coma> <digits> <coma> <digits> <coma> <digits> <lineEnd>
+<notEOL>        ~ [^\n]*
+<coma>          ~ ','
+<value>         ~ <notEOL>
+<comment>       ~ <coma> <notEOL>
+<Intray>        ~ <escape>'c'[\d]
+
+escape          ~ [\x{1b}]
+digits          ~ [\d]+
+X               ~ digits
+Y               ~ digits
+length          ~ digits
+thick           ~ digits
+#shade          ~ digits
+lineEnd         ~ [\n]
+SOURCE
 
 sub escapetext {
     my $text = shift;
@@ -17,11 +107,11 @@ sub escapetext {
 
 sub escape($block,$escape){
     return $block if $escape eq "\x{1b}";
-    return $block =~ s/$escape/\x{1b}/gr;
+    return $block =~ s/$escape(?=[\d\+a-z])/\x{1b}/gr;
 }
 
 sub limpia($tira,$escape = "\x{1b}",$done = '') {
- return $done.escape($tira,$escape) unless $tira =~ /(:?$escape\+X[^\n]*\n)|(:?=UDK=(?!=|U|D|K|,|¨U|¨D|¨K]).)/;
+ return $done.escape($tira,$escape) unless $tira =~ /(:?$escape\+X[^\n]*\n)|(:?=UDK=(?!=|U|D|K|,|¨U|Ü|¨D|¨K).)/;
  my $do = $PREMATCH;
  $tira  = $POSTMATCH;
  if ('=' eq substr($MATCH,0,1)) {
@@ -29,12 +119,17 @@ sub limpia($tira,$escape = "\x{1b}",$done = '') {
      $done .= escape($do,$escape);
      $escape = $esc;
   } else {
-     my $ending = "\x{1b}".substr($MATCH,1);; 
+     my $ending = "\x{1b}".substr($MATCH,1);
      $done .= escape($do,$escape).$ending;
      $escape = "\x{1b}"
   }  
   return limpia($tira,$escape,$done);
 }
+
+my $g = Marpa::R2::Scanless::G->new({
+    action_object => 'main',
+    default_action => 'do_first_arg',
+    source         => \$gramar});
 
 1;
 __END__
--- t/01_UDK.t
+++ t/01_UDK.t
@@ -1,9 +1,14 @@
 use strict;
 use Test::More 0.98;
 use xes2pdf;
-is(xes2pdf::escape("1234","\x{1b}"),"1234");
-is(xes2pdf::escape("1234","2"),"1\x{1b}34");
-is(xes2pdf::limpia("=UDK=&&1\n"),"\x{1b}1\n");
+is(xes2pdf::escape("1234","\x{1b}"),"1234","unchanged if \$escape=\\x{1b}" );
+is(xes2pdf::escape("1234","2"),"1\x{1b}34","changed all \$escape for \\x{1b}");
+is(xes2pdf::limpia("=UDK=&&1\n"),"\x{1b}1\n","=UDK= defines \$escape char");
+is(xes2pdf::limpia("=UDK=&&+Xoo=UDK=l&&&1\n&&&"),"\x{1b}+Xoo=UDK=l&&&1\n&&&","\$escape+X undefines  \$escape char");
+is(xes2pdf::limpia("=UDK=&&+Xoo=UDK=l&&&1\n=UDK=&&1"),"\x{1b}+Xoo=UDK=l&&&1\n\x{1b}1","second =UDK= defines  \$escape char");
+is(xes2pdf::limpia("=UDK=$_"),"=UDK=$_","forbiden \"$_\"")for ("=","\n","U","D","K","Ü","¨U","¨D","¨K",",");
+is(xes2pdf::limpia("=UDK=&=UDK=UDK=b=&=UDK=&"),"=UDK=&","test =UDK=# print");
+
 
 done_testing;
 
